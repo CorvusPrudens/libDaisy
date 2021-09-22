40929c395e46b6fbf7d383ff199bbc75738e34e0
CorvusPrudens <CorvusPrudens@users.noreply.github.com>
2021-09-22 17:07:50 +0000

    deploy: d931bf1a2b9c870581a4c3bff5ef77285d08f3a8

--- search/all_12.js
+++ search/all_12.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['randomnumbergenerator_0',['RandomNumberGenerator',['../classdaisy_1_1_random_number_generator.html',1,'daisy']]],
+  ['random_0',['Random',['../classdaisy_1_1_random.html',1,'daisy']]],
   ['raw_5fdata_1',['raw_data',['../structdaisy_1_1_wav_file_info.html#ad1e3c8f4c720cbbdfbdf26b5a2ea2903',1,'daisy::WavFileInfo']]],
   ['rawstate_2',['RawState',['../classdaisy_1_1_switch.html#a77496bb98f169ba613821e2f1b7fc243',1,'daisy::Switch']]],
   ['read_3',['Read',['../classdaisy_1_1_ring_buffer.html#a112c8553ddc197ad491be9bd8ffabaf9',1,'daisy::RingBuffer::Read()'],['../classdaisy_1_1_ring_buffer_3_01_t_00_010_01_4.html#a5135f3de87d57d04dbde0ab633370251',1,'daisy::RingBuffer&lt; T, 0 &gt;::Read()']]],
@@ -11,7 +11,7 @@ var searchData=
   ['read_5fid_5fcmd2_8',['READ_ID_CMD2',['../group__flash.html#gac5ec3b44ff1efb54410cac6d360d50db',1,'READ_ID_CMD2():&#160;flash_IS25LP080D.h'],['../group__flash.html#gac5ec3b44ff1efb54410cac6d360d50db',1,'READ_ID_CMD2():&#160;flash_IS25LP064A.h']]],
   ['read_5fmanufact_5fand_5fid_9',['READ_MANUFACT_AND_ID',['../group__flash.html#ga11e012cf5dd37a7ca010df9be2e5c9a8',1,'READ_MANUFACT_AND_ID():&#160;flash_IS25LP080D.h'],['../group__flash.html#ga11e012cf5dd37a7ca010df9be2e5c9a8',1,'READ_MANUFACT_AND_ID():&#160;flash_IS25LP064A.h']]],
   ['read_5fread_5fparam_5freg_5fcmd_10',['READ_READ_PARAM_REG_CMD',['../group__flash.html#ga222437d6975ac6ea75f60867cff60554',1,'flash_IS25LP080D.h']]],
-  ['read_5fserial_5fflash_5fdisco_5fparam_5fcmd_11',['READ_SERIAL_FLASH_DISCO_PARAM_CMD',['../group__flash.html#ga216fff040d8bab7b5df5591ebfc2c5d6',1,'READ_SERIAL_FLASH_DISCO_PARAM_CMD():&#160;flash_IS25LP080D.h'],['../group__flash.html#ga216fff040d8bab7b5df5591ebfc2c5d6',1,'READ_SERIAL_FLASH_DISCO_PARAM_CMD():&#160;flash_IS25LP064A.h']]],
+  ['read_5fserial_5fflash_5fdisco_5fparam_5fcmd_11',['READ_SERIAL_FLASH_DISCO_PARAM_CMD',['../group__flash.html#ga216fff040d8bab7b5df5591ebfc2c5d6',1,'READ_SERIAL_FLASH_DISCO_PARAM_CMD():&#160;flash_IS25LP064A.h'],['../group__flash.html#ga216fff040d8bab7b5df5591ebfc2c5d6',1,'READ_SERIAL_FLASH_DISCO_PARAM_CMD():&#160;flash_IS25LP080D.h']]],
   ['read_5fstatus_5freg_5fcmd_12',['READ_STATUS_REG_CMD',['../group__flash.html#gaa3c6b4a899c5b0875dfcf503f1e41b63',1,'READ_STATUS_REG_CMD():&#160;flash_IS25LP064A.h'],['../group__flash.html#gaa3c6b4a899c5b0875dfcf503f1e41b63',1,'READ_STATUS_REG_CMD():&#160;flash_IS25LP080D.h']]],
   ['read_5funique_5fid_13',['READ_UNIQUE_ID',['../group__flash.html#gab326593933b21da8d3fb2765aca1d282',1,'READ_UNIQUE_ID():&#160;flash_IS25LP064A.h'],['../group__flash.html#gab326593933b21da8d3fb2765aca1d282',1,'READ_UNIQUE_ID():&#160;flash_IS25LP080D.h']]],
   ['readable_14',['readable',['../classdaisy_1_1_ring_buffer_3_01_t_00_010_01_4.html#af813b904145707cef9794fbd8346f066',1,'daisy::RingBuffer&lt; T, 0 &gt;::readable()'],['../classdaisy_1_1_ring_buffer.html#a7055d8829240337fc8979fa3eba2341c',1,'daisy::RingBuffer::readable()']]],
@@ -22,8 +22,8 @@ var searchData=
   ['receivecallback_19',['ReceiveCallback',['../class_usb_handle.html#a33ca9123252e485a3d2045590e5e6217',1,'UsbHandle::ReceiveCallback()'],['../class_usb_handle.html#a33ca9123252e485a3d2045590e5e6217',1,'UsbHandle::ReceiveCallback()']]],
   ['receivedma_20',['ReceiveDma',['../classdaisy_1_1_i2_c_handle.html#ad6f3fdb091285a40a34ca2ebf09a94a8',1,'daisy::I2CHandle']]],
   ['rectangle_21',['Rectangle',['../classdaisy_1_1_rectangle.html',1,'daisy']]],
-  ['red_22',['Red',['../classdaisy_1_1_color.html#ab645aa34a553a044e9c901546e78ed8f',1,'daisy::Color']]],
-  ['red_23',['RED',['../classdaisy_1_1_color.html#af93d31d5fc63705c228bf9da407c9a40a8b805ea394bedf4786fd371ea10283b8',1,'daisy::Color']]],
+  ['red_22',['RED',['../classdaisy_1_1_color.html#af93d31d5fc63705c228bf9da407c9a40a8b805ea394bedf4786fd371ea10283b8',1,'daisy::Color']]],
+  ['red_23',['Red',['../classdaisy_1_1_color.html#ab645aa34a553a044e9c901546e78ed8f',1,'daisy::Color']]],
   ['relcardadd_24',['RelCardAdd',['../struct_d_s_y___s_d___card_info_type_def.html#a5c530fb4ff9a3032125d75a44e9b0a0a',1,'DSY_SD_CardInfoTypeDef']]],
   ['remove_25',['Remove',['../classdaisy_1_1_f_i_f_o_base.html#a7a040559220ea37332b62fbb9dc82e7d',1,'daisy::FIFOBase::Remove()'],['../classdaisy_1_1_stack_base.html#a0aca4cedbec62035867fff63a2be529f',1,'daisy::StackBase::Remove()']]],
   ['removeallequalto_26',['RemoveAllEqualTo',['../classdaisy_1_1_f_i_f_o_base.html#aa3f1d45b20a1d63789b4346bbfec5404',1,'daisy::FIFOBase::RemoveAllEqualTo()'],['../classdaisy_1_1_stack_base.html#a5c246e49801ddb0c162567a6cdc84a58',1,'daisy::StackBase::RemoveAllEqualTo()']]],
@@ -36,7 +36,7 @@ var searchData=
   ['resettobootloader_33',['ResetToBootloader',['../classdaisy_1_1_system.html#ae7e0bb7a214d85ceaa2264f2d00afd5a',1,'daisy::System']]],
   ['resettodefault_34',['ResetToDefault',['../classdaisy_1_1_mapped_value.html#a60288a18a6173903f3a7e4c5c9aebb84',1,'daisy::MappedValue::ResetToDefault()'],['../classdaisy_1_1_mapped_float_value.html#accc506533ee066815a8c78f3b7a6cbfc',1,'daisy::MappedFloatValue::ResetToDefault()'],['../classdaisy_1_1_mapped_int_value.html#a921f370452a295e5f52f0483b3559a83',1,'daisy::MappedIntValue::ResetToDefault()'],['../classdaisy_1_1_mapped_string_list_value.html#aa1edf81d83cf2ce90d56d0f36f7a50a9',1,'daisy::MappedStringListValue::ResetToDefault()']]],
   ['restart_35',['Restart',['../classdaisy_1_1_wav_player.html#ac3b2f9f9cdf18ea861be227dcffd5da9',1,'daisy::WavPlayer']]],
-  ['result_36',['Result',['../classdaisy_1_1_timer_handle.html#a7425f809b115688f70d7a12acd5a16c7',1,'daisy::TimerHandle::Result()'],['../classdaisy_1_1_wm8731.html#acee74b7ae4c637c45d33afcb91da7988',1,'daisy::Wm8731::Result()'],['../class_usb_handle.html#a676c11695fe5f925515eaa3d3e2d53a6',1,'UsbHandle::Result()'],['../class_usb_handle.html#a676c11695fe5f925515eaa3d3e2d53a6',1,'UsbHandle::Result()'],['../classdaisy_1_1_dac_handle.html#a66e1c535c1809b56744d842005008d54',1,'daisy::DacHandle::Result()'],['../classdaisy_1_1_i2_c_handle.html#a4896eede457dea6ed2ed68d9b06e8e16',1,'daisy::I2CHandle::Result()'],['../classdaisy_1_1_sai_handle.html#a5463939a5d7fe1a363cfb9beb602d468',1,'daisy::SaiHandle::Result()'],['../classdaisy_1_1_sdmmc_handler.html#ad3aa247c4e2a3e8da344a64673768970',1,'daisy::SdmmcHandler::Result()'],['../classdaisy_1_1_spi_handle.html#acee8aad7e8008a2dbdfc8f260712bab7',1,'daisy::SpiHandle::Result()'],['../classdaisy_1_1_uart_handler.html#ac45c7ac82e6f0328b4e7b03fd12e7237',1,'daisy::UartHandler::Result()'],['../classdaisy_1_1_wav_writer.html#ab6477ad91d6d9f4804ac9150edd33e1b',1,'daisy::WavWriter::Result()']]],
+  ['result_36',['Result',['../classdaisy_1_1_uart_handler.html#ac45c7ac82e6f0328b4e7b03fd12e7237',1,'daisy::UartHandler::Result()'],['../classdaisy_1_1_wm8731.html#acee74b7ae4c637c45d33afcb91da7988',1,'daisy::Wm8731::Result()'],['../class_usb_handle.html#a676c11695fe5f925515eaa3d3e2d53a6',1,'UsbHandle::Result()'],['../class_usb_handle.html#a676c11695fe5f925515eaa3d3e2d53a6',1,'UsbHandle::Result()'],['../classdaisy_1_1_dac_handle.html#a66e1c535c1809b56744d842005008d54',1,'daisy::DacHandle::Result()'],['../classdaisy_1_1_i2_c_handle.html#a4896eede457dea6ed2ed68d9b06e8e16',1,'daisy::I2CHandle::Result()'],['../classdaisy_1_1_sai_handle.html#a5463939a5d7fe1a363cfb9beb602d468',1,'daisy::SaiHandle::Result()'],['../classdaisy_1_1_sdmmc_handler.html#ad3aa247c4e2a3e8da344a64673768970',1,'daisy::SdmmcHandler::Result()'],['../classdaisy_1_1_spi_handle.html#acee8aad7e8008a2dbdfc8f260712bab7',1,'daisy::SpiHandle::Result()'],['../classdaisy_1_1_timer_handle.html#a7425f809b115688f70d7a12acd5a16c7',1,'daisy::TimerHandle::Result()'],['../classdaisy_1_1_wav_writer.html#ab6477ad91d6d9f4804ac9150edd33e1b',1,'daisy::WavWriter::Result()']]],
   ['rgbled_37',['RgbLed',['../classdaisy_1_1_rgb_led.html',1,'daisy']]],
   ['right_38',['right',['../namespacedaisy.html#ac734c565c1dc55cbf6b74ea942f40c1ca7c4f29407893c334a6cb7a87bf045c0d',1,'daisy']]],
   ['ring_5fled_39',['ring_led',['../classdaisy_1_1_daisy_petal.html#a24abaac2327c0057f99b5d5a4f15bd5a',1,'daisy::DaisyPetal']]],
@@ -53,6 +53,7 @@ var searchData=
   ['ringbuffer_3c_20t_2c_200_20_3e_50',['RingBuffer&lt; T, 0 &gt;',['../classdaisy_1_1_ring_buffer_3_01_t_00_010_01_4.html',1,'daisy']]],
   ['ringled_51',['RingLed',['../classdaisy_1_1_daisy_petal.html#a6b6b85167d6938e7b4d151599773d682',1,'daisy::DaisyPetal']]],
   ['risingedge_52',['RisingEdge',['../classdaisy_1_1_encoder.html#ab501cf73d10d81058b485c48b9eb506f',1,'daisy::Encoder::RisingEdge()'],['../classdaisy_1_1_switch.html#a71f6054840c76ebe3cb2517dc7830d34',1,'daisy::Switch::RisingEdge()']]],
-  ['rx_53',['rx',['../structdaisy_1_1_uart_handler_1_1_config.html#adb318e26f13636e1bee740fa47497392',1,'daisy::UartHandler::Config']]],
-  ['rxactive_54',['RxActive',['../classdaisy_1_1_uart_handler.html#aeeb804c58c2ccf574142b2a030d2d83b',1,'daisy::UartHandler']]]
+  ['rx_53',['RX',['../classdaisy_1_1_spi_handle.html#aa7c484882faf187c7c4153db7cf5e4daaf9c24782c24c237d16e79f18e2fa9046',1,'daisy::SpiHandle']]],
+  ['rx_54',['rx',['../structdaisy_1_1_uart_handler_1_1_config.html#adb318e26f13636e1bee740fa47497392',1,'daisy::UartHandler::Config']]],
+  ['rxactive_55',['RxActive',['../classdaisy_1_1_uart_handler.html#aeeb804c58c2ccf574142b2a030d2d83b',1,'daisy::UartHandler']]]
 ];
